089521f482edc896fa59afffc6db31e7fd36c1bd
Gustavo <ggu3131@gmail.com>
2023-03-27 06:58:53 -0300

    testes unitarios

--- project/urls.py
+++ project/urls.py
@@ -1,4 +1,4 @@
-"""projeto URL Configuration
+"""project URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.1/topics/http/urls/
@@ -13,12 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import \
+    static  # É para configura arquivos estáticos nas urls
 from django.contrib import admin
-from django.urls import path, include
-from django.conf.urls.static import static #É para configura arquivos estáticos nas urls
+from django.urls import include, path
 
-from django.conf import settings 
-# from projeto import settings, faz a mesma coisa q a linha de cima, porem o django indica usar o modo django.config (igual na linha de cima)
+# from project import settings, faz a mesma coisa q a linha de cima, porem o django indica usar o modo django.config (igual na linha de cima)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -27,4 +28,3 @@ urlpatterns = [
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-
--- recipes/tests.py
+++ recipes/tests.py
@@ -1,3 +1,36 @@
 from django.test import TestCase
+from django.urls import resolve, reverse
 
-# Create your tests here.
+from recipes import views
+
+# class RecipeURLsTest(TestCase):
+#     def test_recipe_home_urls_is_correct(self):
+#         url = reverse('recipes:home')
+#         self.assertEqual(url, '/')
+
+#     def test_recipe_category_urls_is_correct(self):
+#         url = reverse('recipes:category', kwargs={"category_id": 1})
+#         self.assertEqual(url, '/recipes/category/1/')
+
+#     def teste_recipe_detail_url_is_correct(self):
+#         url = reverse('recipes:recipe', args=(1,))
+#         self.assertEqual(url, "/recipes/1/")
+
+
+class RecipeViewsTest(TestCase):
+    def teste_recipe_home_view_function_is_correct(self):
+        view = resolve(reverse('recipes:home'))
+        self.assertIs(view.func, views.home)
+
+    def teste_recipe_category_view_function_is_correct(self):
+        view = resolve(
+            reverse('recipes:category', kwargs={"category_id": 1})
+        )
+
+        self.assertIs(view.func, views.category)
+
+    def teste_recipe_detail_view_function_is_correct(self):
+        view = resolve(
+            reverse('recipes:recipe', kwargs={'id': 1})
+        )
+        self.assertIs(view.func, views.recipe)
